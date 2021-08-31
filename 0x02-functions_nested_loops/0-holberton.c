a7d32d539c753ebcff744de18b789fdd507760e3
alx-pre_course <biniyamgetaneh30@gmail.com>
2021-08-31 08:33:43 -0700

    Mandatory

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: nothing, return void
+ * main -  prints Holberton, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i; /*Declaring statements*/
-	char string[9] = "Holberton"; /*Arrays*/
-	for (i = 0 ; i < 9 ; i++) /*Start for*/
-	{
-		_putchar(string[i]);/*display word*/
-	} /*End for*/
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
+
 	return (0);
 }
